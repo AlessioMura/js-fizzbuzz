7a2e7ea6b49930ee5de564ecd02a2512a0a69ebb
AlessioMura <alessiomura03@gmail.com>
2024-10-01 17:00:10 +0200

    final fix

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ for (let i = 1; i < 101; i++) {
     console.log("fizz");
   } else if (i % 5 === 0) {
     console.log("buzz");
-  } else if (!i % 3 === 0 && !i % 5 === 0) {
+  } else {
     console.log(i);
   }
 }
